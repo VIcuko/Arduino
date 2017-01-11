e96ba9affaaa57d84c2c9a390edb3658d8c00c04
Vicuko <victorc.fernandez@gmail.com>
2017-01-11 18:26:30 +0100

    Create Practice 2: Make 4 LED blink in order

--- Practice_2/Practice_2.ino
+++ Practice_2/Practice_2.ino
@@ -1,12 +1,30 @@
+int tiempo=200;
+
 void setup() {
-  pinMode(13, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(13,HIGH);
-  delay(500);
-  digitalWrite(13,LOW);
-  delay(100);  
+  digitalWrite(5,HIGH);
+  delay(tiempo);
+  digitalWrite(5,LOW);
+  delay(tiempo);
+  digitalWrite(6,HIGH);
+  delay(tiempo);
+  digitalWrite(6,LOW);
+  delay(tiempo);
+  digitalWrite(7,HIGH);
+  delay(tiempo);
+  digitalWrite(7,LOW);
+  delay(tiempo);
+  digitalWrite(8,HIGH);
+  delay(tiempo);
+  digitalWrite(8,LOW);
+  delay(tiempo);
+  
 }
 
 
